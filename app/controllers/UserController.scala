6a7f61429d1d26ea10f7c0912701ae98af682336
Sean Lee <sumioturk@Seans-MacBook-Pro.local>
2013-04-12 09:55:16 +0900

    async user api

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -7,6 +7,10 @@ import com.sumioturk.satomi.infrastrucure.MongoRepository
 import com.mongodb.casbah.MongoConnection
 import com.mongodb.{CommandResult, WriteResult}
 import java.util.UUID
+import play.api.libs.concurrent.Akka
+import play.api.Play.current
+import scala.concurrent.ExecutionContext
+import ExecutionContext.Implicits.global
 
 /**
  * (C) Copyright 2013 OMCAS Inc.
@@ -33,29 +37,41 @@ object UserController extends Controller {
             name = name.foldLeft("")(_ + _),
             isGay = isGay.foldLeft("")(_ + _) == "true"
           )
-          userRepo.store(user)
-          Ok(convertToJson(user))
+          val promiseOfWriteResult = Akka.future(userRepo.store(user))
+          Async {
+            promiseOfWriteResult.map(wr => Ok(convertToJson(user)))
+          }
         case _ =>
           Forbidden("Invalid Params")
       }
   }
 
   def find(id: String) = Action {
-    userRepo.resolve(id) match {
-      case None =>
-        NotFound("Not Fount %s".format(id))
-      case Some(user) =>
-        Ok(convertToJson(user))
+    Async {
+      Akka.future(userRepo.resolve(id)) map {
+        user: Option[User] =>
+          user match {
+            case None =>
+              NotFound("Not Found %s".format(id))
+            case Some(user) =>
+              Ok(convertToJson(user))
+          }
+      }
     }
   }
 
   def remove(id: String) = Action {
-    userRepo.resolve(id) match {
-      case None =>
-        NotFound("Not Found %s".format(id))
-      case Some(user) =>
-        userRepo.remove(user)
-        Ok("Deleted %s".format(id))
+    Async {
+      Akka.future(userRepo.resolve(id)) map {
+        userOpt: Option[User] =>
+          userOpt match {
+            case None =>
+              NotFound("Not Found %s".format(id))
+            case Some(user) =>
+              userRepo.remove(user)
+              Ok("Deleted %s".format(id))
+          }
+      }
     }
   }
 
